d33b77762b4c82fa781b1743d423f9d0055142f1
MichinariNukazawa <michinari.nukazawa@gmail.com>
2018-03-14 21:48:08 +0900

    fix language code literal to functional variable

--- overwrite/js/language.js
+++ overwrite/js/language.js
@@ -1,6 +1,14 @@
 'use strict';
 
 /*export default*/ class Language{
+	static get_code(){
+		return 'en';
+	}
+
+	static get_code_by_google(){
+		return 'en';
+	}
+
 	static command(keyword){
 		if(0 === keyword.indexOf(":license")){
 			let str = read_textfile('dictionary/english/LICENSE.md');
